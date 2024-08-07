47b4bc64b3b0072da52a9ccbfb259a30e657dec6
Florian Robel <f.robel@shopware.com>
2024-08-07 15:13:56 +0200

    feat: Add possibility to use custom customer for Login task

--- src/tasks/shop-customer/Account/Login.ts
+++ src/tasks/shop-customer/Account/Login.ts
@@ -1,12 +1,14 @@
 import { test as base } from '@playwright/test';
 import type { Task } from '../../../types/Task';
 import type { FixtureTypes} from '../../../types/FixtureTypes';
+import type { Customer } from '../../../types/ShopwareTypes';
 
 export const Login = base.extend<{ Login: Task }, FixtureTypes>({
-    Login: async ({ ShopCustomer, DefaultSalesChannel, StorefrontAccountLogin, StorefrontAccount }, use)=> {
-        const task = () => {
+    Login: async ({ ShopCustomer, DefaultSalesChannel, StorefrontAccountLogin, StorefrontAccount }, use) => {
+        const task = (customCustomer?: Customer) => {
             return async function Login() {
-                const { customer } = DefaultSalesChannel;
+
+                const customer = customCustomer ? customCustomer : DefaultSalesChannel.customer;
 
                 await ShopCustomer.goesTo(StorefrontAccountLogin.url());
 
