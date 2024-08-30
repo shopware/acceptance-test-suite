45e19a0645b76165e32204d4aa3616f6ed8070c2
Florian Robel <f.robel@shopware.com>
2024-08-30 15:55:09 +0200

    feat: add locators to home and product detail page

--- src/page-objects/storefront/Home.ts
+++ src/page-objects/storefront/Home.ts
@@ -4,9 +4,11 @@ import type { PageObject } from '../../types/PageObject';
 export class Home implements PageObject {
 
     public readonly productImages: Locator;
+    public readonly productListItems: Locator;
 
     constructor(public readonly page: Page) {
         this.productImages = page.locator('.product-image-link');
+        this.productListItems = page.getByRole('listitem');
     }
 
     url() {
