366d1486ceb1844608e1286e8773e4c5f70eb5cb
Philipp Schuch <p.schuch@shopware.com>
2024-11-07 13:39:11 +0100

    fix: Cart quantity select locator

--- src/page-objects/storefront/CheckoutCart.ts
+++ src/page-objects/storefront/CheckoutCart.ts
@@ -21,7 +21,7 @@ export class CheckoutCart implements PageObject {
         this.stockReachedAlert = page.getByText('only available 1 times');
         this.cartLineItemImages = page.locator('.line-item-img-link');
         this.unitPriceInfo = page.locator('.line-item-unit-price-value');
-        this.cartQuantityNumber = page.getByLabel('Quantity', { exact: true });
+        this.cartQuantityNumber = page.locator('input[name="quantity"]');
     }
 
     url() {
