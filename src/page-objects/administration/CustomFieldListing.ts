cb0477cb6926811850b55cd9dd6cfe560a906598
Florian Robel <97941660+frobel@users.noreply.github.com>
2024-11-21 10:12:03 +0100

    feat: add custom field page objects (#174)

---
+++ src/page-objects/administration/CustomFieldListing.ts
@@ -0,0 +1,37 @@
+import type { Page, Locator } from '@playwright/test';
+import type { PageObject } from '../../types/PageObject';
+
+export class CustomFieldListing implements PageObject {
+    public readonly addNewSetButton: Locator;
+    public readonly customFieldRows: Locator;
+
+    constructor(public readonly page: Page) {
+        this.addNewSetButton = page.getByText('Add new set');
+        this.customFieldRows = page.getByRole('row');
+    }
+
+    async getLineItemByCustomFieldSetName(customFieldSetName: string): Promise<Record<string, Locator>> {
+        const lineItem = this.page.getByRole('row').filter({ hasText: customFieldSetName });
+        const customFieldSetNameText = lineItem.getByText(customFieldSetName);
+        const customFieldSetContextButton = lineItem.locator('.sw-context-button__button');
+        const customFieldSetEditButton = this.page.locator('.sw-context-menu__content').getByRole('link', { name: 'Edit'});
+        const customFieldSetDeleteButton = this.page.locator('.sw-context-menu__content').getByRole('link', { name: 'Delete'});
+        const warningDialog = this.page.getByRole('dialog', { name: 'Warning' });
+        const warningDialogCancelButton = warningDialog.getByRole('button', { name: 'Cancel' });
+        const warningDialogDeleteButton = warningDialog.getByRole('button', { name: 'Delete' });
+
+        return {
+            customFieldSetNameText: customFieldSetNameText,
+            customFieldSetContextButton: customFieldSetContextButton,
+            customFieldSetEditButton: customFieldSetEditButton,
+            customFieldSetDeleteButton: customFieldSetDeleteButton,
+            warningDialog: warningDialog,
+            warningDialogCancelButton: warningDialogCancelButton,
+            warningDialogDeleteButton: warningDialogDeleteButton,
+        }
+    }
+
+    url() {
+        return `#/sw/settings/custom/field/index`
+    }
+}
--- src/page-objects/administration/RuleCreate.ts
+++ src/page-objects/administration/RuleCreate.ts
@@ -1,13 +1,15 @@
 import type { Page, Locator } from '@playwright/test';
 import type { PageObject } from '../../types/PageObject';
 
-export class RuleDetail implements PageObject {
+export class RuleCreate implements PageObject {
 
     public readonly nameInput: Locator;
     public readonly priorityInput: Locator;
     public readonly conditionTypeSelectionInput: Locator;
     public readonly conditionValueSelectionInput: Locator;
     public readonly filtersResultPopoverSelectionList: Locator;
+    public readonly saveButton: Locator;
+    public readonly cancelButton: Locator;
 
     constructor(public readonly page: Page) {
         this.nameInput = page.getByLabel('Name');
@@ -15,9 +17,11 @@ export class RuleDetail implements PageObject {
         this.conditionTypeSelectionInput = page.locator('.sw-condition-type-select').locator('.sw-single-select__selection');
         this.conditionValueSelectionInput = page.locator('.sw-condition__value-content').locator('.sw-entity-single-select__selection');
         this.filtersResultPopoverSelectionList = page.locator('.sw-select-result-list__content').getByRole('listitem');
+        this.saveButton = page.getByRole('button', { name: 'Save' });
+        this.cancelButton = page.getByRole('button', { name: 'Cancel' });
     }
 
-    url(ruleId: string, tabName = 'base') {
-        return `#/sw/settings/rule/detail/${ruleId}/${tabName}`
+    url() {
+        return `#/sw/settings/rule/create/base`
     }
 }
