2bc993dfc8b20d98534a16d470455c21f430a29c
Marcel Kräml <m.kraeml@shopware.com>
2024-11-25 15:00:11 +0100

    test increased timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,38 +10,38 @@ process.env['APP_URL'] = process.env['APP_URL'] ?? defaultAppUrl;
 // make sure APP_URL ends with a slash
 process.env['APP_URL'] = (process.env['APP_URL'] ?? '').replace(/\/+$/, '') + '/';
 if (process.env['ADMIN_URL']) {
-  process.env['ADMIN_URL'] = process.env['ADMIN_URL'].replace(/\/+$/, '') + '/';
+    process.env['ADMIN_URL'] = process.env['ADMIN_URL'].replace(/\/+$/, '') + '/';
 } else {
-  process.env['ADMIN_URL'] = process.env['APP_URL'] + 'admin/';
+    process.env['ADMIN_URL'] = process.env['APP_URL'] + 'admin/';
 }
 
 export default defineConfig({
-  testDir: './tests',
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  timeout: 60000,
-  retries: 0,
-  workers: process.env.CI ? 2 : 1,
-  reporter: process.env.CI ? [
-    ['html'],
-    ['github'],
-  ] : 'html',
-  use: {
-    baseURL: process.env['APP_URL'],
-    trace: 'retain-on-failure',
-    video: 'off',
-  },
-  // We abuse this to wait for the external webserver
-  webServer: {
-    command: process.env['APP_URL'] === defaultAppUrl ? 'docker compose up --pull=always --quiet-pull shopware' : 'sleep 1h',
-    url: process.env['APP_URL'],
-    reuseExistingServer: true,
-    timeout: 180000,
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+    testDir: './tests',
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    timeout: 1200000,
+    retries: 0,
+    workers: process.env.CI ? 2 : 1,
+    reporter: process.env.CI ? [
+        ['html'],
+        ['github'],
+    ] : 'html',
+    use: {
+        baseURL: process.env['APP_URL'],
+        trace: 'retain-on-failure',
+        video: 'off',
     },
-  ],
+    // We abuse this to wait for the external webserver
+    webServer: {
+        command: process.env['APP_URL'] === defaultAppUrl ? 'docker compose up --pull=always --quiet-pull shopware' : 'sleep 1h',
+        url: process.env['APP_URL'],
+        reuseExistingServer: true,
+        timeout: 180000,
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
+    ],
 });
