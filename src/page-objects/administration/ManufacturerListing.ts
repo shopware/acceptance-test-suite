798ca5c86a05c47ea02eccd9af7f158fe413d6ea
Florian Robel <f.robel@shopware.com>
2024-11-25 14:45:20 +0100

    feat: add manufacturer page objects

---
+++ src/page-objects/administration/ManufacturerListing.ts
@@ -0,0 +1,41 @@
+import type { Page, Locator } from '@playwright/test';
+import type { PageObject } from '../../types/PageObject';
+
+export class ManufacturerListing implements PageObject {
+    public readonly addManufacturerButton: Locator;
+    public readonly manufacturerRows: Locator;
+
+    constructor(public readonly page: Page) {
+        this.addManufacturerButton = page.getByText('Add manufacturer');
+        this.manufacturerRows = page.getByRole('row');
+    }
+
+    async getLineItemByManufacturerName(manufacturerName: string): Promise<Record<string, Locator>> {
+        const lineItem = this.page.getByRole('row').filter({ hasText: manufacturerName });
+        const manufacturerNameText = lineItem.getByText(manufacturerName);
+        const manufacturerWebsiteText = lineItem.locator('.sw-data-grid__cell--link');
+        const manufacturerCheckbox = lineItem.locator('.sw-data-grid__cell--selection');
+        const manufacturerContextButton = lineItem.locator('.sw-context-button__button');
+        const manufacturerEditButton = this.page.locator('.sw-context-menu__content').getByRole('link', { name: 'Edit'});
+        const manufacturerDeleteButton = this.page.locator('.sw-context-menu__content').getByRole('link', { name: 'Delete'});
+        const warningDialog = this.page.getByRole('dialog', { name: 'Warning' });
+        const warningDialogCancelButton = warningDialog.getByRole('button', { name: 'Cancel' });
+        const warningDialogDeleteButton = warningDialog.getByRole('button', { name: 'Delete' });
+
+        return {
+            manufacturerNameText: manufacturerNameText,
+            manufacturerWebsiteText: manufacturerWebsiteText,
+            manufacturerCheckbox: manufacturerCheckbox,
+            manufacturerContextButton: manufacturerContextButton,
+            manufacturerEditButton: manufacturerEditButton,
+            manufacturerDeleteButton: manufacturerDeleteButton,
+            warningDialog: warningDialog,
+            warningDialogCancelButton: warningDialogCancelButton,
+            warningDialogDeleteButton: warningDialogDeleteButton,
+        }
+    }
+
+    url() {
+        return `#/sw/manufacturer/index`
+    }
+}
