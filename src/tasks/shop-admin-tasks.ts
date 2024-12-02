f75d1d8ffd2007d42564ea05db161af813b6bf38
vanpham-sw <d.vanpham@shopware.com>
2024-12-02 10:23:52 +0700

    fix: move and stabilize CreateLinkTypeCategory task

--- src/tasks/shop-admin-tasks.ts
+++ src/tasks/shop-admin-tasks.ts
@@ -2,8 +2,10 @@ import { mergeTests } from '@playwright/test';
 
 import { SaveProduct } from './shop-admin/Product/SaveProduct';
 import { ExpectNotification } from './shop-admin/ExpectNotification';
+import { CreateLinkTypeCategory } from './shop-admin/Category/CreateLinkTypeCategory';
 
 export const test = mergeTests(
     SaveProduct,
-    ExpectNotification
+    ExpectNotification,
+    CreateLinkTypeCategory,
 );
