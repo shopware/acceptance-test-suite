f87b1cc88374c4691c2ec39d38b2fd405d1bd40a
Daniel Meyer <dmeyer@shopware.com>
2024-12-06 21:10:57 +0100

    feat: Add PaymentDetail page object

---
+++ src/page-objects/administration/PaymentDetail.ts
@@ -0,0 +1,20 @@
+import type { Page, Locator } from '@playwright/test';
+import type { PageObject } from '../../types/PageObject';
+
+export class PaymentDetail implements PageObject {
+
+    public readonly header: Locator;
+    public readonly nameField: Locator;
+    public readonly availabilityRule: Locator;
+
+
+    constructor(public readonly page: Page) {
+        this.header = page.locator('.smart-bar__header');
+        this.nameField = page.getByLabel('Name').first();
+        this.availabilityRule = page.locator('.sw-settings-payment-detail__field-availability-rule').locator('.sw-entity-single-select__selection-text');
+    }
+
+    url(paymentId: string) {
+        return `#/sw/settings/payment/detail/${paymentId}`;
+    }
+}
