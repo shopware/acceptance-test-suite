cd28445777f8a99b786484cb3a7e8d4bb49b6c51
Daniel Meyer <dmeyer@shopware.com>
2024-12-13 10:51:34 +0100

    feat: add RuleType and RuleAssignmentEntity to Shopware Types

--- src/page-objects/administration/RuleDetail.ts
+++ src/page-objects/administration/RuleDetail.ts
@@ -10,12 +10,16 @@ export class RuleDetail extends RuleCreate implements PageObject {
     public readonly shippingMethodAvailabilityRulesCardEmptyState: Locator;
     public readonly shippingMethodAvailabilityRulesCardSearchField: Locator;
     public readonly taxProviderRulesCard: Locator;
+    public readonly taxProviderRulesCardEmptyState: Locator;
     public readonly paymentMethodsAvailabilityRulesCard: Locator;
     public readonly paymentMethodsAvailabilityRulesCardEmptyState: Locator;
     public readonly paymentMethodsAvailabilityRulesCardLink: Locator;
     public readonly promotionOrderRulesCard: Locator;
+    public readonly promotionOrderRulesCardEmptyState: Locator;
     public readonly promotionCustomerRulesCard: Locator;
+    public readonly promotionCustomerRulesCardEmptyState: Locator;
     public readonly promotionCartRulesCard: Locator;
+    public readonly promotionCartRulesCardEmptyState: Locator;
 
     constructor(public readonly page: Page) {
         super(page);
@@ -25,12 +29,16 @@ export class RuleDetail extends RuleCreate implements PageObject {
         this.shippingMethodAvailabilityRulesCardEmptyState = this.shippingMethodAvailabilityRulesCard.getByRole('alert');
         this.shippingMethodAvailabilityRulesCardSearchField = this.shippingMethodAvailabilityRulesCard.getByPlaceholder('Search...');
         this.taxProviderRulesCard = page.locator('.sw-settings-rule-detail-assignments__card-tax_provider');
+        this.taxProviderRulesCardEmptyState = this.taxProviderRulesCard.getByRole('alert');
         this.paymentMethodsAvailabilityRulesCard = page.locator('.sw-settings-rule-detail-assignments__card-payment_method');
-        this.paymentMethodsAvailabilityRulesCardEmptyState = page.locator('.sw-settings-rule-detail-assignments__card-payment_method').getByRole('alert');
+        this.paymentMethodsAvailabilityRulesCardEmptyState = this.paymentMethodsAvailabilityRulesCard.getByRole('alert');
         this.paymentMethodsAvailabilityRulesCardLink = page.locator('.sw-settings-rule-detail-assignments__card-payment_method').getByRole('link');
         this.promotionOrderRulesCard = page.locator('.sw-settings-rule-detail-assignments__card-promotion_order_rule');
+        this.promotionOrderRulesCardEmptyState = this.promotionOrderRulesCard.getByRole('alert');
         this.promotionCustomerRulesCard = page.locator('.sw-settings-rule-detail-assignments__card-promotion_customer_rule');
+        this.promotionCustomerRulesCardEmptyState = this.promotionCustomerRulesCard.getByRole('alert');
         this.promotionCartRulesCard = page.locator('.sw-settings-rule-detail-assignments__card-promotion_cart_rule');
+        this.promotionCartRulesCardEmptyState = this.promotionCartRulesCard.getByRole('alert');
     }
 
     async getEntityCard(cardLocator: Locator): Promise<Record<string, Locator>> {
--- src/tasks/shop-admin/Rule/AssignEntitiesToRule.ts
+++ src/tasks/shop-admin/Rule/AssignEntitiesToRule.ts
@@ -1,15 +1,12 @@
 import {Locator, test as base} from '@playwright/test';
 import type {Task} from '../../../types/Task';
 import type {FixtureTypes} from '../../../types/FixtureTypes';
+import {RuleType} from '../../../types/ShopwareTypes';
+import {RuleAssignmentEntity} from '../../../types/ShopwareTypes';
 
-interface Entity {
-    id: string;
-    name: string;
-    ruleType: string;
-}
 export const AssignEntitiesToRule = base.extend<{ AssignEntitiesToRule: Task }, FixtureTypes>({
     AssignEntitiesToRule: async ({ AdminRuleDetail }, use ) => {
-        const task = (assignableEntities: Entity[]) => {
+        const task = (assignableEntities: RuleAssignmentEntity[]) => {
             return async function AssignEntitiesToRule() {
                 async function entityAssignment(entityName: string, card: Locator) {
                         const entityCard = await AdminRuleDetail.getEntityCard(card);
@@ -19,35 +16,27 @@ export const AssignEntitiesToRule = base.extend<{ AssignEntitiesToRule: Task }, 
                         await AdminRuleDetail.page.locator('.sw-button--primary').getByText('Add').click();
                 }
                 for (const entity of assignableEntities) {
-                    const validRuleType = {
-                        shipping1: 'shippingMethodAvailabilityRule',
-                        tax1: 'taxProviderRule',
-                        payment1: 'paymentMethodAvailabilityRule',
-                        promotion1: 'promotionOrderRule',
-                        promotion2: 'promotionCustomerRule',
-                        promotion3: 'promotionCartRule',
-                    };
                     switch (entity.ruleType) {
-                        case validRuleType.shipping1:
+                        case RuleType.shippingAvailability:
                             await entityAssignment(entity.name, AdminRuleDetail.shippingMethodAvailabilityRulesCard);
                             break;
-                        case validRuleType.tax1:
+                        case RuleType.taxAvailability:
                             await entityAssignment(entity.name, AdminRuleDetail.taxProviderRulesCard);
                             break;
-                        case validRuleType.payment1:
+                        case RuleType.paymentAvailability:
                             await entityAssignment(entity.name, AdminRuleDetail.paymentMethodsAvailabilityRulesCard);
                             break;
-                        case validRuleType.promotion1:
+                        case RuleType.promotionOrder:
                             await entityAssignment(entity.name, AdminRuleDetail.promotionOrderRulesCard);
                             break;
-                        case validRuleType.promotion2:
+                        case RuleType.promotionCustomer:
                             await entityAssignment(entity.name, AdminRuleDetail.promotionCustomerRulesCard);
                             break;
-                        case validRuleType.promotion3:
+                        case RuleType.promotionCart:
                             await entityAssignment(entity.name, AdminRuleDetail.promotionCartRulesCard);
                             break;
                         default:
-                            throw new Error(`Unknown rule type "${entity.ruleType}". Valid rule types: "${Object.values(validRuleType).join('", "')}".`);
+                            throw new Error(`Unknown rule type "${entity.ruleType}". Valid rule types: "${Object.values(RuleType).join('", "')}".`);
                     }
                 }
             }
