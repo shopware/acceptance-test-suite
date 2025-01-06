beace44203639f62b38f8cadf52bd1f98b196aab
Florian Robel <f.robel@shopware.com>
2025-01-06 10:35:21 +0100

    fix: add additional locator for smtp input fields within first run wizard page object

--- src/page-objects/administration/FirstRunWizard.ts
+++ src/page-objects/administration/FirstRunWizard.ts
@@ -29,8 +29,21 @@ export class FirstRunWizard implements PageObject {
     public readonly salesChannelSelectionList: Locator;
     public readonly salesChannelSelectionMultiSelect: Locator;
     public readonly smtpServerTitle: Locator;
+
+    /**
+     * @deprecated - Use `smtpServerFieldInputs` instead.
+     */
     public readonly smtpServerFields: Locator;
     public readonly smtpServerFieldInputs: Locator;
+    public readonly smtpServerHostInput: Locator;
+    public readonly smtpServerPortInput: Locator;
+    public readonly smtpServerUsernameInput: Locator;
+    public readonly smtpServerPasswordInput: Locator;
+    public readonly smtpServerEncryptionInput: Locator;
+    public readonly smtpServerSenderAddressInput: Locator;
+    public readonly smtpServerDeliveryAddressInput: Locator;
+    public readonly smtpServerDisableEmailDeliveryCheckbox: Locator;
+
     public readonly payPalPaymethods: Locator;
     public readonly payPalInfoCard: Locator;
     public readonly emailAddressInputField: Locator;
@@ -80,7 +93,20 @@ export class FirstRunWizard implements PageObject {
         this.smtpServerButton = page.getByText('Configure own SMTP server', { exact: true });
         this.smtpServerTitle = page.getByText('SMTP server', { exact: true });
         this.smtpServerFields = page.locator('.sw-field');
-        this.smtpServerFieldInputs = page.locator('.mt-field');
+        this.smtpServerHostInput = page.getByLabel('Host');
+        this.smtpServerPortInput = page.getByLabel('Port');
+        this.smtpServerUsernameInput = page.getByLabel('Username');
+        this.smtpServerPasswordInput = page.getByLabel('Password');
+        this.smtpServerEncryptionInput = page.locator('.sw-single-select__selection-input');
+        this.smtpServerSenderAddressInput = page.getByLabel('Sender address');
+        this.smtpServerDeliveryAddressInput = page.getByLabel('Delivery address');
+        this.smtpServerDisableEmailDeliveryCheckbox = page.getByLabel('Disable email delivery');
+
+        if (satisfies(instanceMeta.version, '<6.7')) {
+            this.smtpServerFieldInputs = page.locator('.sw-field');
+        } else {
+            this.smtpServerFieldInputs = page.locator('.mt-field');
+        }
 
         // PayPal part
         this.payPalSetupHeader = page.locator('.sw-modal__title', { hasText: 'Setup PayPal'});
