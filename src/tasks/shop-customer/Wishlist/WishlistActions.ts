afe7e688a533a363b70041df0f0305eb9e5e1340
Orkun Cavli <o.cavli@shopware.com>
2025-02-04 13:24:37 +0100

    feat: fix wishlist actions

---
+++ src/tasks/shop-customer/Wishlist/WishlistActions.ts
@@ -0,0 +1,48 @@
+import { test as base } from '@playwright/test';
+import type { Task } from '../../../types/Task';
+import type { FixtureTypes } from '../../../types/FixtureTypes';
+import { Product } from '../../../types/ShopwareTypes';
+
+export const AddProductToCartFromWishlist = base.extend<{ AddProductToCartFromWishlist: Task }, FixtureTypes>({
+    AddProductToCartFromWishlist: async ({ ShopCustomer, StorefrontWishlist }, use) => {
+        const task = (ProductData: Product) => {
+            return async function AddProductToCart() {
+                const listedItem = await StorefrontWishlist.getListingItemByProductId(ProductData.id);
+                await listedItem.productAddToShoppingCart.click();
+                await StorefrontWishlist.page.waitForResponse((response) => response.url().includes(`checkout/offcanvas`) && response.ok());
+                await ShopCustomer.expects(StorefrontWishlist.offCanvasCartTitle).toBeVisible();
+                await ShopCustomer.expects(StorefrontWishlist.offCanvasCart.getByText(ProductData.name)).toBeVisible();
+            }
+        };
+
+        await use(task);
+    },
+});
+
+export const RemoveProductFromWishlist = base.extend<{ RemoveProductFromWishlist: Task }, FixtureTypes>({
+    RemoveProductFromWishlist: async ({ StorefrontHome , StorefrontWishlist}, use) => {
+        const task = (ProductData: Product) => {
+            return async function AddProductToWishlist() {
+                const listedItem = await StorefrontHome.getListingItemByProductId(ProductData.id);
+                await listedItem.wishlistAddedIcon.click();
+                await StorefrontWishlist.page.waitForResponse((response) => response.url().includes(`remove/${ProductData.id}`) && response.ok());
+            }
+        };
+
+        await use(task);
+    },
+});
+
+export const AddProductToWishlist = base.extend<{ AddProductToWishlist: Task }, FixtureTypes>({
+    AddProductToWishlist: async ({ StorefrontHome , ShopCustomer}, use) => {
+        const task = (ProductData: Product) => {
+            return async function AddProductToWishlist() {
+                const listedItem = await StorefrontHome.getListingItemByProductId(ProductData.id);
+                await listedItem.wishlistNotAddedIcon.click();
+                await ShopCustomer.expects(listedItem.wishlistAddedIcon).toBeVisible();
+            }
+        };
+
+        await use(task);
+    },
+});
