9532a881ea19dbc05dc3a3f418f14464ee182569
Florian Robel <97941660+frobel@users.noreply.github.com>
2025-02-05 14:54:17 +0100

    fix: add locators for listing price to existing page objects (#304)

--- src/page-objects/storefront/Home.ts
+++ src/page-objects/storefront/Home.ts
@@ -99,6 +99,9 @@ export class Home implements PageObject {
         const productAddToShoppingCart = listingItem.getByRole('button', {
             name: 'Add to shopping cart',
         });
+        const productListingPrice = listingItem.locator('.list-price-price');
+        const productListingPricePercentage = listingItem.locator('.list-price-percentage');
+        const productListingPriceBadge = listingItem.locator('.badge-discount');
 
         return {
             productImage: productImage,
@@ -110,6 +113,9 @@ export class Home implements PageObject {
             productPrice: productPrice,
             productName: productName,
             productAddToShoppingCart: productAddToShoppingCart,
+            productListingPrice: productListingPrice,
+            productListingPricePercentage: productListingPricePercentage,
+            productListingPriceBadge: productListingPriceBadge,
         };
     }
 
