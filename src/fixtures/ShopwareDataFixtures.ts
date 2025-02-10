b4415946986efd11c58d05e2bff30876d47d63e4
Daniel Galla <d.galla@imi.de>
2025-02-10 11:35:31 +0100

    feat: move sales channel default data to fixtures (#305)
    ...move shipping method to fixture
    ...move payment method to fixture
    ...move language to fixture
    ...move currency to fixture
    ...move tax to fixture
    ...move country to fixture
    ...move snippet set to fixture
    ...move theme to fixture

---
+++ src/fixtures/ShopwareDataFixtures.ts
@@ -0,0 +1,146 @@
+import { test as base } from '@playwright/test';
+import type { FixtureTypes } from '../types/FixtureTypes';
+import {
+    getCountryId,
+    getCurrency,
+    getLanguageData,
+    getPaymentMethodId,
+    getShippingMethodId,
+    getSnippetSetId,
+    getTaxId,
+    getThemeId,
+} from '../services/ShopwareDataHelpers';
+
+export interface Country {
+    id: string;
+}
+
+export interface Currency {
+    id: string;
+}
+
+export interface Language {
+    id: string;
+    translationCode: { id: string };
+}
+
+export interface PaymentMethod {
+    id: string;
+}
+
+export interface ShippingMethod {
+    id: string;
+}
+
+export interface SnippetSet {
+    id: string;
+}
+
+export interface Tax {
+    id: string;
+}
+
+export interface Theme {
+    id: string;
+}
+
+export interface ShopwareDataFixtureTypes {
+    Country: Country;
+    Currency: Currency;
+    Language: Language;
+    PaymentMethod: PaymentMethod;
+    ShippingMethod: ShippingMethod;
+    SnippetSet: SnippetSet;
+    Tax: Tax;
+    Theme: Theme;
+}
+
+export const test = base.extend<NonNullable<unknown>, FixtureTypes>({
+
+    Country: [
+        async ({ AdminApiContext }, use) => {
+            const countryId = await getCountryId('de', AdminApiContext);
+
+            await use({
+                id: countryId,
+            });
+        },
+        { scope: 'worker' },
+    ],
+
+    Currency: [
+        async ({ AdminApiContext }, use) => {
+            const currency = await getCurrency('EUR', AdminApiContext);
+
+            await use({
+                id: currency.id,
+            });
+        },
+        { scope: 'worker' },
+    ],
+
+    Language: [
+        async ({ AdminApiContext }, use) => {
+            const language = await getLanguageData('en-GB', AdminApiContext);
+
+            await use(language);
+        },
+        { scope: 'worker' },
+    ],
+
+    PaymentMethod: [
+        async ({ AdminApiContext }, use) => {
+            const paymentMethodId = await getPaymentMethodId(AdminApiContext);
+
+            await use({
+                id: paymentMethodId,
+            });
+        },
+        { scope: 'worker' },
+    ],
+
+    ShippingMethod: [
+        async ({ AdminApiContext }, use) => {
+            const shippingMethodId = await getShippingMethodId('Standard', AdminApiContext);
+
+            await use({
+                id: shippingMethodId,
+            });
+        },
+        { scope: 'worker' },
+    ],
+
+    SnippetSet: [
+        async ({ AdminApiContext }, use) => {
+            const snippedSetId = await getSnippetSetId('en-GB', AdminApiContext);
+
+            await use({
+                id: snippedSetId,
+            });
+        },
+        { scope: 'worker' },
+    ],
+
+    Tax: [
+        async ({ AdminApiContext }, use) => {
+            const taxId = await getTaxId(AdminApiContext);
+
+            await use({
+                id: taxId,
+            });
+        },
+        { scope: 'worker' },
+    ],
+
+    Theme: [
+        async ({ AdminApiContext }, use) => {
+            const themeId = await getThemeId('Storefront', AdminApiContext);
+
+            await use({
+                id: themeId,
+            });
+        },
+        { scope: 'worker' },
+    ],
+
+});
