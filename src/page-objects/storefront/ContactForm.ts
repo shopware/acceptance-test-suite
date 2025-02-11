2a8550ab35b98999189c0c8c22a8c235a85c4ad4
Florian Robel <97941660+frobel@users.noreply.github.com>
2025-02-11 08:15:29 +0100

    fix: make contact form locators compatible with 6.7. and 6.6. (#309)
    * fix: make contact form locators compatible with 6.7. and 6.6.
    
    * fix: adjust contact form locators

--- src/page-objects/storefront/ContactForm.ts
+++ src/page-objects/storefront/ContactForm.ts
@@ -1,10 +1,19 @@
 import type { Page, Locator } from '@playwright/test';
 import type { PageObject } from '../../types/PageObject';
 import { Home } from './Home';
+import { HelperFixtureTypes } from '../../fixtures/HelperFixtures';
+import { satisfies } from 'compare-versions';
 
 export class ContactForm extends Home implements PageObject {
+    /**
+     * @deprecated Compatible until 6.6.x, will be removed in 6.8.x, use 'contactWrapper' instead
+     */
     public readonly contactModal: Locator;
-    public readonly contactSuccessModal: Locator;
+    /**
+     * @deprecated Compatible until 6.6.x, will be removed in 6.8.x, use 'contactSuccessMessage' instead
+     */
+    public readonly contactSuccessModal: Locator | undefined;
+    public readonly contactWrapper: Locator;
     public readonly salutationSelect: Locator;
     public readonly firstNameInput: Locator;
     public readonly lastNameInput: Locator;
@@ -16,6 +25,8 @@ export class ContactForm extends Home implements PageObject {
     public readonly submitButton: Locator;
     public readonly contactSuccessMessage: Locator;
     public readonly cardTitle: Locator;
+    public readonly formFieldFeedback: Locator | undefined;
+    public readonly formAlert: Locator | undefined;
     /**
      *  Captcha locators
      */
@@ -27,28 +38,39 @@ export class ContactForm extends Home implements PageObject {
     public readonly greCaptchaV2Input: Locator;
     public readonly greCaptchaProtectionInformation: Locator;
 
-    constructor(public readonly page: Page) {
+    constructor(public readonly page: Page, public readonly instanceMeta: HelperFixtureTypes['InstanceMeta']) {
         super(page);
-        this.contactModal = this.page.getByRole('dialog').filter({ has: this.page.getByLabel('Contact', { exact: true }) });
-        this.salutationSelect = this.contactModal.getByLabel('Salutation*');
-        this.firstNameInput = this.contactModal.getByLabel('First name*');
-        this.lastNameInput = this.contactModal.getByLabel('Last name*');
-        this.emailInput = this.contactModal.getByLabel('Your email address*');
-        this.phoneInput = this.contactModal.getByLabel('Phone*');
-        this.subjectInput = this.contactModal.getByLabel('Subject*');
-        this.commentInput = this.contactModal.getByLabel('Comment*');
-        this.privacyPolicyCheckbox = this.contactModal.getByRole('checkbox', { name: 'By selecting continue you confirm that you have read and agree to our' });
-        this.submitButton = this.contactModal.getByRole('button', { name: 'Submit' });
-        this.contactSuccessModal = this.page.getByRole('dialog').filter({ has: this.page.locator('.confirm-message') });
-        this.contactSuccessMessage = this.contactSuccessModal.locator('.confirm-message');
-        this.cardTitle = this.contactModal.locator('.card-title');
-        this.basicCaptcha = this.contactModal.locator('.basic-captcha');
+
+        this.contactModal = this.page.getByRole('dialog').filter({ has: this.page.getByText('Contact') });
+
+        if (satisfies(instanceMeta.version, '<6.7')) {
+            this.contactWrapper = this.page.getByRole('dialog').filter({ has: this.page.getByText('Contact') });
+            this.contactSuccessModal = this.page.getByRole('dialog').filter({ has: this.page.locator('.confirm-message') });
+            this.contactSuccessMessage = this.contactSuccessModal.locator('.confirm-message');
+        } else {
+            this.contactWrapper = this.page.locator('.card').filter({ has: this.page.getByText('Contact') });
+            this.formFieldFeedback = this.contactWrapper.locator('.form-field-feedback');
+            this.formAlert = this.page.getByRole('alert');
+            this.contactSuccessMessage = this.page.locator('.confirm-message');
+        }
+        this.basicCaptcha = this.contactWrapper.locator('.basic-captcha');
+        this.salutationSelect = this.contactWrapper.getByLabel('Salutation');
+        this.firstNameInput = this.contactWrapper.getByLabel('First name');
+        this.lastNameInput = this.contactWrapper.getByLabel('Last name');
+        this.emailInput = this.contactWrapper.getByLabel('Your email address');
+        this.phoneInput = this.contactWrapper.getByLabel('Phone');
+        this.subjectInput = this.contactWrapper.getByLabel('Subject');
+        this.commentInput = this.contactWrapper.getByLabel('Comment');
+        this.privacyPolicyCheckbox = this.contactWrapper.getByRole('checkbox', { name: 'By selecting continue you confirm that you have read and agree to our' });
+        this.submitButton = this.contactWrapper.getByRole('button', { name: 'Submit' });
+        this.cardTitle = this.contactWrapper.locator('.card-title');
+        this.basicCaptcha = this.contactWrapper.locator('.basic-captcha');
+        this.greCaptchaV2Container = this.contactWrapper.locator('.grecaptcha-v2-container');
+        this.greCaptchaV2Input = this.contactWrapper.locator('.grecaptcha-v2-input');
+        this.greCaptchaProtectionInformation = this.contactWrapper.locator('.grecaptcha-protection-information');
         this.basicCaptchaImage = this.basicCaptcha.locator('img');
         this.basicCaptchaRefreshButton = this.basicCaptcha.locator('.basic-captcha-content-refresh-icon');
         this.basicCaptchaInput = this.basicCaptcha.locator('input[name="shopware_basic_captcha_confirm"]');
-        this.greCaptchaV2Container = this.contactModal.locator('.grecaptcha-v2-container');
-        this.greCaptchaV2Input = this.contactModal.locator('.grecaptcha-v2-input');
-        this.greCaptchaProtectionInformation = this.contactModal.locator('.grecaptcha-protection-information');
     }
 
     url() {
