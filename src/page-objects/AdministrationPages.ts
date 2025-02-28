5d898135755886f5839dcd81607dd8167a89850f
Daniel Meyer <d.meyer@shopware.com>
2025-02-28 15:24:21 +0100

    fix: add CreateCustomFieldSet test compatibility for 6.7

--- src/page-objects/AdministrationPages.ts
+++ src/page-objects/AdministrationPages.ts
@@ -194,8 +194,8 @@ export const test = base.extend<FixtureTypes>({
         await use(new CustomFieldCreate(AdminPage));
     },
 
-    AdminCustomFieldDetail: async ({ AdminPage }, use) => {
-        await use(new CustomFieldDetail(AdminPage));
+    AdminCustomFieldDetail: async ({ AdminPage, InstanceMeta }, use) => {
+        await use(new CustomFieldDetail(AdminPage, InstanceMeta));
     },
 
     AdminRuleCreate: async ({ AdminPage }, use) => {
