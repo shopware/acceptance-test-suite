ebe0fc6b858018cec7905c90af5b7365a98ef574
Daniel Meyer <d.meyer@shopware.com>
2025-03-03 14:43:04 +0100

    fix: add locator and module to RuleCreate

--- src/page-objects/administration/RuleCreate.ts
+++ src/page-objects/administration/RuleCreate.ts
@@ -1,5 +1,7 @@
 import type { Page, Locator } from '@playwright/test';
 import type { PageObject } from '../../types/PageObject';
+import { getSelectFieldListitem } from './modules/SelectFieldListitem';
+import { HelperFixtureTypes } from '../../fixtures/HelperFixtures';
 
 export class RuleCreate implements PageObject {
 
@@ -10,8 +12,9 @@ export class RuleCreate implements PageObject {
     public readonly filtersResultPopoverSelectionList: Locator;
     public readonly saveButton: Locator;
     public readonly cancelButton: Locator;
+    public readonly valueNotAvailableTooltip: Locator;
 
-    constructor(public readonly page: Page) {
+    constructor(public readonly page: Page, public readonly instanceMeta: HelperFixtureTypes['InstanceMeta']) {
         this.nameInput = page.getByLabel('Name');
         this.priorityInput = page.getByLabel('Priority');
         this.conditionTypeSelectionInput = page.locator('.sw-condition-type-select').locator('.sw-single-select__selection');
@@ -19,9 +22,13 @@ export class RuleCreate implements PageObject {
         this.filtersResultPopoverSelectionList = page.locator('.sw-select-result-list__content').getByRole('listitem');
         this.saveButton = page.getByRole('button', { name: 'Save' });
         this.cancelButton = page.getByRole('button', { name: 'Cancel' });
+        this.valueNotAvailableTooltip = page.locator('.sw-tooltip');
     }
 
     url() {
         return `#/sw/settings/rule/create/base`
+    }
+    async getSelectFieldListitem(selectField: Locator, listItem: string) {
+        return getSelectFieldListitem(this.page, selectField, listItem, this.instanceMeta);
     }
 }
