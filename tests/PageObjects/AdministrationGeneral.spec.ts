56975af197422242c11faf8eef5fe99445a2ad08
Patrick Weyck <p.weyck@shopware.com>
2025-03-04 15:51:10 +0100

    fix: flakiness

--- tests/PageObjects/AdministrationGeneral.spec.ts
+++ tests/PageObjects/AdministrationGeneral.spec.ts
@@ -16,12 +16,11 @@ test('Administration page objects - General.', async ({
     AdminProductDetail,
 }) => {
     await ShopAdmin.goesTo(AdminCustomerListing.url());
+    await ShopAdmin.expects(AdminCustomerListing.headline).toBeVisible();
     await ShopAdmin.expects(AdminCustomerListing.addCustomerButton).toBeVisible();
 
-    await ShopAdmin.goesTo(AdminManufacturerListing.url());
-    await ShopAdmin.expects(AdminManufacturerListing.addManufacturerButton).toBeVisible();
-
     await ShopAdmin.goesTo(AdminManufacturerCreate.url());
+    await ShopAdmin.expects(AdminManufacturerCreate.nameInput).toBeVisible();
     await ShopAdmin.expects(AdminManufacturerCreate.saveButton).toBeVisible();
 
     await ShopAdmin.goesTo(AdminCustomerDetail.url(DefaultSalesChannel.customer.id));
@@ -33,16 +32,22 @@ test('Administration page objects - General.', async ({
     await ShopAdmin.expects(AdminCategories.getTreeItemContextButton(category.name)).toBeVisible();
 
     await ShopAdmin.goesTo(AdminLandingPageCreate.url());
+    await ShopAdmin.expects(AdminLandingPageCreate.nameInput).toBeVisible();
     await ShopAdmin.expects(AdminLandingPageCreate.saveLandingPageButton).toBeVisible();
 
     const product = await TestDataService.createBasicProduct();
     const customer = await TestDataService.createCustomer();
     const order = await TestDataService.createOrder([{ product, quantity: 1 }], customer);
     await ShopAdmin.goesTo(AdminOrderDetail.url(order.id));
+    await ShopAdmin.expects(AdminOrderDetail.dataGridContextButton).toBeVisible();
     await ShopAdmin.expects(AdminOrderDetail.saveButton).toBeVisible();
 
     await ShopAdmin.goesTo(AdminProductDetail.url(product.id));
     await ShopAdmin.expects(AdminProductDetail.savePhysicalProductButton).toBeVisible();
+    await ShopAdmin.expects(AdminProductDetail.stockInput).toBeVisible();
+
+    await ShopAdmin.goesTo(AdminManufacturerListing.url());
+    await ShopAdmin.expects(AdminManufacturerListing.addManufacturerButton).toBeVisible();
 
     // eslint-disable-next-line playwright/no-conditional-in-test
     if (!InstanceMeta.isSaaS) {
--- tests/PageObjects/AdministrationSettings.spec.ts
+++ tests/PageObjects/AdministrationSettings.spec.ts
@@ -18,28 +18,35 @@ test('Administration page objects - Settings.', async ({
     AdminRuleCreate,
 }) => {
     await ShopAdmin.goesTo(AdminCustomerGroupListing.url());
+    await ShopAdmin.expects(AdminCustomerGroupListing.headline).toBeVisible();
     await ShopAdmin.expects(AdminCustomerGroupListing.addCustomerGroupButton).toBeVisible();
 
     await ShopAdmin.goesTo(AdminCustomerGroupCreate.url());
+    await ShopAdmin.expects(AdminCustomerGroupCreate.cardTitle).toBeVisible();
     await ShopAdmin.expects(AdminCustomerGroupCreate.saveButton).toBeVisible();
 
     await ShopAdmin.goesTo(AdminCustomerGroupDetail.url(DefaultSalesChannel.salesChannel.customerGroupId));
+    await ShopAdmin.expects(AdminCustomerGroupDetail.headline).toBeVisible();
     await ShopAdmin.expects(AdminCustomerGroupDetail.saveButton).toBeVisible();
 
     await ShopAdmin.goesTo(AdminFlowBuilderCreate.url());
+    await ShopAdmin.expects(AdminFlowBuilderCreate.header).toBeVisible();
     await ShopAdmin.expects(AdminFlowBuilderCreate.saveButton).toBeVisible();
 
-    await ShopAdmin.goesTo(AdminFlowBuilderListing.url());
-    await ShopAdmin.expects(AdminFlowBuilderListing.createFlowButton).toBeVisible();
+    await ShopAdmin.goesTo(AdminRuleCreate.url());
+    await ShopAdmin.expects(AdminRuleCreate.nameInput).toBeVisible();
+    await ShopAdmin.expects(AdminRuleCreate.saveButton).toBeVisible();
 
     const flowId = await getFlowId('Order enters status unconfirmed', AdminApiContext);
     await ShopAdmin.goesTo(AdminFlowBuilderDetail.url(flowId));
+    await ShopAdmin.expects(AdminFlowBuilderDetail.generalTab).toBeVisible();
     await ShopAdmin.expects(AdminFlowBuilderDetail.saveButton).toBeVisible();
 
     await ShopAdmin.goesTo(AdminCustomFieldListing.url());
     await ShopAdmin.expects(AdminCustomFieldListing.addNewSetButton).toBeVisible();
 
     await ShopAdmin.goesTo(AdminCustomFieldCreate.url());
+    await ShopAdmin.expects(AdminCustomFieldCreate.technicalNameInput).toBeVisible();
     await ShopAdmin.expects(AdminCustomFieldCreate.saveButton).toBeVisible();
 
     // eslint-disable-next-line playwright/no-conditional-in-test
@@ -52,9 +59,10 @@ test('Administration page objects - Settings.', async ({
         }
 
         await ShopAdmin.goesTo(AdminFirstRunWizard.url());
+        await ShopAdmin.expects(AdminFirstRunWizard.welcomeText).toBeVisible();
         await ShopAdmin.expects(AdminFirstRunWizard.nextButton).toBeVisible({ timeout: 15_000 });
-    }
 
-    await ShopAdmin.goesTo(AdminRuleCreate.url());
-    await ShopAdmin.expects(AdminRuleCreate.saveButton).toBeVisible();
+        await ShopAdmin.goesTo(AdminFlowBuilderListing.url());
+        await ShopAdmin.expects(AdminFlowBuilderListing.createFlowButton).toBeVisible();
+    }
 });
