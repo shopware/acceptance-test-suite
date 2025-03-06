afa9a51ec79322d692fbce5efb8ee69b4ce3de88
Yusuf  Tür <106602505+yusufttur@users.noreply.github.com>
2025-03-06 10:43:04 +0100

    fix: locator compatibility for release date input (#353)

--- src/page-objects/administration/ProductDetail.ts
+++ src/page-objects/administration/ProductDetail.ts
@@ -104,7 +104,11 @@ export class ProductDetail implements PageObject {
         this.saleChannelsInput = page.locator('.sw-product-detail__select-visibility');
 
         // Labelling
-        this.releaseDateInput = page.locator('.sw-product-detail-base__labelling-card').locator('.form-control');
+        if (satisfies(instanceMeta.version, '<6.7')) {
+            this.releaseDateInput = page.locator('.sw-product-detail-base__labelling-card').locator('.form-control');
+        } else {
+            this.releaseDateInput = page.locator('.dp__input_readonly');
+        }
 
         // Media upload interactions
         this.uploadMediaButton = page.getByRole('button', { name: 'Upload file' });
