cbf0ab5d4bf2b7c0d152add5d768d062d2f2c607
Orkun Cavli <o.cavli@shopware.com>
2025-07-03 09:41:25 +0200

    fix: add all tabs to the admin order detail page

--- src/page-objects/administration/OrderDetail.ts
+++ src/page-objects/administration/OrderDetail.ts
@@ -10,6 +10,13 @@ export class OrderDetail implements PageObject {
     public readonly orderTag: Locator;
     public readonly itemsCardHeader: Locator;
 
+    /**
+     * Tabs
+     */
+    public readonly generalTabLink: Locator;
+    public readonly detailsTabLink: Locator;
+    public readonly documentsTabLink: Locator;
+
     constructor(public readonly page: Page, public readonly instanceMeta: HelperFixtureTypes['InstanceMeta']) {
         this.saveButton = page.locator('.sw-order-detail__smart-bar-save-button');
         this.dataGridContextButton = page.locator('.sw-data-grid__actions-menu').and(page.getByRole('button'));
@@ -19,6 +26,11 @@ export class OrderDetail implements PageObject {
         } else {
             this.itemsCardHeader = page.locator('.mt-card__header').getByText('Items');
         }
+
+        //Tabs
+        this.generalTabLink = page.getByRole('tab', { name: 'General' });
+        this.detailsTabLink = page.getByRole('tab', { name: 'Details' });
+        this.documentsTabLink = page.getByRole('tab', { name: 'Documents' });
     }
 
     url(orderId: string, tabName = 'general') {
