65c39a8b8b2e8d4807e04acacec94fcaffecec89
Daniel Meyer <d.meyer@shopware.com>
2025-07-23 12:33:31 +0200

    remove old approach from HelperFixtures

--- src/fixtures/HelperFixtures.ts
+++ src/fixtures/HelperFixtures.ts
@@ -18,14 +18,7 @@ export interface HelperFixtureTypes {
         features: FeaturesType,
     },
     HideElementsForScreenshot: (page: Page, selectors: string[]) => Promise<void>,
-    ReplaceElementsForScreenshot: (page: Page, selectors: string[]) => Promise<void>,
-    GetScreenshotDimensions: (page: Page, options: {
-        requestURL?: string,
-        width?: number,
-        scrollableElementVertical?: string,
-        scrollableElementHorizontal?: string,
-        additionalHeight?: number
-    }) => Promise<void>,
+    ReplaceElementsForScreenshot: (page: Page, selectors: string[]) => Promise<void>,  
 }
 
 export const test = base.extend<NonNullable<unknown>, FixtureTypes>({
@@ -109,8 +102,8 @@ export const test = base.extend<NonNullable<unknown>, FixtureTypes>({
                 if (!selectors.length) return;
 
                 const css = selectors
-                    .map(selector => `${selector} { visibility: hidden !important; }`)
-                    .join('\n');
+                .map(selector => `${selector} { visibility: hidden !important; }`)
+                .join('\n');
 
                 await page.addStyleTag({ content: css });
             };
@@ -128,75 +121,24 @@ export const test = base.extend<NonNullable<unknown>, FixtureTypes>({
      */
 
     ReplaceElementsForScreenshot : [
-        async ({}, use) => {
-            const fn = async (page: Page, selectors: string[]) => {
-                if (!selectors.length) return;
+    async ({}, use) => {
+        const fn = async (page: Page, selectors: string[]) => {
+        if (!selectors.length) return;
 
-                await page.evaluate((selectors: string[]) => {
-                    selectors.forEach(selector => {
-                        // @ts-expect-error no DOM types in this context
-                        const elements = document.querySelectorAll(selector);
-                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                        elements.forEach((el: any) => {
-                            el.textContent = '***';
-                        });
-                    });
-                }, selectors);
-            };
+        await page.evaluate((selectors: string[]) => {
+            selectors.forEach(selector => {
+            // @ts-expect-error no DOM types in this context
+            const elements = document.querySelectorAll(selector);
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            elements.forEach((el: any) => {
+                el.textContent = '***';
+            });
+            });
+        }, selectors);
+        };
 
-            await use(fn);
-        },
+        await use(fn);
+    },
         { scope: 'worker' },
-    ],
-
-    /**
-     * Sets the viewport size for screenshots, ensuring that all scrollable content is visible.
-     * @param page - The Playwright page object to set the viewport size on.
-     * @param options - Optional parameters to customize the viewport size:
-     *  requestURL: string - The URL to wait for before setting the viewport size (default is 'api/notification/message?limit=5' and should be replaced because it takes 3-5 seconds).
-     *  width: number - The width of the viewport (default is 1440).
-     *  scrollableElementVertical: string - The CSS selector for the scrollable element whose height will be used to set the viewport size (default is '.sw-card-view__content').
-     *  scrollableElementHorizontal: string - The CSS selector for the scrollable element whose width will be used to set the viewport size (default is '.sw-data-grid__wrapper').
-     *  additionalHeight: number - Additional height to add to the viewport size (default is 0).
-     */
-    GetScreenshotDimensions: [
-        async ({ }, use) => {
-            const viewportSize = async (
-                page: Page, options: {
-                    requestURL?: string,
-                    width?: number,
-                    scrollableElementVertical?: string,
-                    scrollableElementHorizontal?: string,
-                    additionalHeight?: number
-                } = {
-                    requestURL: 'api/notification/message?limit=5',
-                    width: 1440,
-                    scrollableElementVertical: '.sw-card-view__content',
-                    scrollableElementHorizontal: '.sw-data-grid__wrapper',
-                    additionalHeight: 0,
-                }) => {
-                await page.waitForResponse(response => response.url().includes(options.requestURL ?? 'api/notification/message?limit=5'));
-                const scrollableElementVertical = page.locator(options.scrollableElementVertical ?? '.sw-card-view__content');
-                let pageHeight = 1080; // Default height
-                if (await scrollableElementVertical.count() > 0) {
-                    pageHeight = await scrollableElementVertical.evaluate(el => el.scrollHeight);
-                }
-                const header = page.locator('.sw-page__head-area');
-                let headerHeight = 0;
-                if (await header.count() > 0) {
-                    headerHeight = await header.evaluate(el => el.offsetHeight);
-                }
-                const scrollableElementHorizontal = page.locator(options.scrollableElementHorizontal ?? '.sw-data-grid__wrapper')
-                let pageWidth = options.width ?? 1440; // Default width
-                if (await scrollableElementHorizontal.count() > 0) {
-                    pageWidth = (await scrollableElementHorizontal.evaluate(el => el.scrollWidth));
-                }
-                await page.setViewportSize({
-                    width: pageWidth,
-                    height: pageHeight + headerHeight + (options.additionalHeight ?? 0) });
-            };
-            await use(viewportSize);
-        },
-        { scope: 'worker' },
-    ],
+    ],      
 });
