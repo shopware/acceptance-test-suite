496a10adada4d9e3ba4116274667eac1477c493c
Daniel Meyer <d.meyer@shopware.com>
2025-07-25 09:45:03 +0200

    remove empty lines

--- src/page-objects/administration/SettingsListing.ts
+++ src/page-objects/administration/SettingsListing.ts
@@ -6,14 +6,10 @@ export class SettingsListing implements PageObject {
     public readonly contentView: Locator;
     public readonly header: Locator;
 
-
-
     constructor(public readonly page: Page) {
         this.header = page.locator('.sw-settings__content-header');
         this.contentView = page.locator('.sw-desktop__content');
-
     }
-
     url() {
         return '#/sw/settings/index/shop';
     }
