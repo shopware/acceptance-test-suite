7ca7725a8989301a43b2841e77d54703bddf4d87
sguder <s.guder@shopware.com>
2025-08-05 15:38:56 +0200

    chore: move everything into one test

--- tests/PageObjects/AdministrationPromotions.spec.ts
+++ tests/PageObjects/AdministrationPromotions.spec.ts
@@ -1,9 +1,11 @@
 import { test } from '../../src';
 
-test('Promotions Listing Page', async ({ 
+test('Promotions Pages', async ({ 
     ShopAdmin, 
     TestDataService, 
     AdminPromotionsListing,
+    AdminPromotionCreate,
+    AdminPromotionDetail,
     
 }) => {
     await test.step('Listing page - Empty state.', async () => {
@@ -15,10 +17,9 @@ test('Promotions Listing Page', async ({
         await ShopAdmin.expects(AdminPromotionsListing.emptyStateAddPromotionButton).toBeVisible();
     });
 
+    const activePromo = await TestDataService.createPromotionWithCode({ active: true });
+    const inactivePromo = await TestDataService.createPromotionWithCode({ active: false });
     await test.step('Listing page - With promotions.', async () => {
-        const activePromo = await TestDataService.createPromotionWithCode({ active: true });
-        const inactivePromo = await TestDataService.createPromotionWithCode({ active: false });
-
         await ShopAdmin.goesTo(AdminPromotionsListing.url());
         await ShopAdmin.expects(AdminPromotionsListing.promotionsTable).toBeVisible();
 
@@ -38,42 +39,33 @@ test('Promotions Listing Page', async ({
         await ShopAdmin.expects(inactiveRow.promotionName).toHaveText(inactivePromo.name);
         await ShopAdmin.expects(inactiveRow.promotionInactive).toBeVisible();
     });
-}); 
 
-test('Promotions Create Page', async ({ 
-    ShopAdmin, 
-    AdminPromotionCreate,
-    
-}) => {
-    await ShopAdmin.goesTo(AdminPromotionCreate.url());
-    await ShopAdmin.expects(AdminPromotionCreate.smartBarHeader).toBeVisible();
-    await ShopAdmin.expects(AdminPromotionCreate.languageSelect).toBeVisible();
-    await ShopAdmin.expects(AdminPromotionCreate.saveButton).toBeVisible();
-    await ShopAdmin.expects(AdminPromotionCreate.cancelButton).toBeVisible();
-    await ShopAdmin.expects(AdminPromotionCreate.generalCard).toBeVisible();
-    await ShopAdmin.expects(AdminPromotionCreate.nameInput).toBeVisible();
-    await ShopAdmin.expects(AdminPromotionCreate.priorityInput).toBeVisible();
-}); 
+    await test.step('Promotions Create Page.', async () => {
+        await ShopAdmin.goesTo(AdminPromotionCreate.url());
+        await ShopAdmin.expects(AdminPromotionCreate.smartBarHeader).toBeVisible();
+        await ShopAdmin.expects(AdminPromotionCreate.languageSelect).toBeVisible();
+        await ShopAdmin.expects(AdminPromotionCreate.saveButton).toBeVisible();
+        await ShopAdmin.expects(AdminPromotionCreate.cancelButton).toBeVisible();
+        await ShopAdmin.expects(AdminPromotionCreate.generalCard).toBeVisible();
+        await ShopAdmin.expects(AdminPromotionCreate.nameInput).toBeVisible();
+        await ShopAdmin.expects(AdminPromotionCreate.priorityInput).toBeVisible();
+    });
 
-test('Promotions Detail Page', async ({ 
-    ShopAdmin,
-    TestDataService,
-    AdminPromotionDetail,
-    
-}) => {
-    const testPromo = await TestDataService.createPromotionWithCode();
-    await ShopAdmin.goesTo(AdminPromotionDetail.url(testPromo.id));
-    await ShopAdmin.expects(AdminPromotionDetail.tabGeneralLink).toBeVisible();
-    await ShopAdmin.expects(AdminPromotionDetail.tabConditionsLink).toBeVisible();
-    await ShopAdmin.expects(AdminPromotionDetail.tabDiscountsLink).toBeVisible();
-    await ShopAdmin.expects(AdminPromotionDetail.promotionCodesCard).toBeVisible();
-    await ShopAdmin.expects(AdminPromotionDetail.promotionCodesHeading).toBeVisible();
-    await ShopAdmin.expects(AdminPromotionDetail.promotionCodesSelection).toBeVisible();
+    await test.step('Promotions Detail Page.', async () => {
+        await ShopAdmin.goesTo(AdminPromotionDetail.url(activePromo.id));
+        await ShopAdmin.expects(AdminPromotionDetail.tabGeneralLink).toBeVisible();
+        await ShopAdmin.expects(AdminPromotionDetail.tabConditionsLink).toBeVisible();
+        await ShopAdmin.expects(AdminPromotionDetail.tabDiscountsLink).toBeVisible();
+        await ShopAdmin.expects(AdminPromotionDetail.promotionCodesCard).toBeVisible();
+        await ShopAdmin.expects(AdminPromotionDetail.promotionCodesHeading).toBeVisible();
+        await ShopAdmin.expects(AdminPromotionDetail.promotionCodesSelection).toBeVisible();
 
-    await ShopAdmin.goesTo(AdminPromotionDetail.url(testPromo.id, 'conditions'));
-    await ShopAdmin.expects(AdminPromotionDetail.preConditionsCard).toBeVisible();
+        await ShopAdmin.goesTo(AdminPromotionDetail.url(activePromo.id, 'conditions'));
+        await ShopAdmin.expects(AdminPromotionDetail.preConditionsCard).toBeVisible();
+        
+        await ShopAdmin.goesTo(AdminPromotionDetail.url(activePromo.id, 'discounts'));
+        await ShopAdmin.expects(AdminPromotionDetail.addDiscountButton).toBeVisible();
+        await ShopAdmin.expects(AdminPromotionDetail.discountCards).toBeVisible();
+    });
     
-    await ShopAdmin.goesTo(AdminPromotionDetail.url(testPromo.id, 'discounts'));
-    await ShopAdmin.expects(AdminPromotionDetail.addDiscountButton).toBeVisible();
-    await ShopAdmin.expects(AdminPromotionDetail.discountCards).toBeVisible();
-}); 
+});
