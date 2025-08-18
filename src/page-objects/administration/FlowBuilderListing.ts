cc27ce79b7141c0813dcfd0bc29e13c8d93abcb6
Daniel Meyer <d.meyer@shopware.com>
2025-08-18 17:41:55 +0200

    fix page error

--- src/page-objects/administration/FlowBuilderListing.ts
+++ src/page-objects/administration/FlowBuilderListing.ts
@@ -2,6 +2,7 @@ import type { Page, Locator } from 'playwright-core';
 import type { PageObject } from '../../types/PageObject';
 
 export class FlowBuilderListing implements PageObject {
+    public readonly page: Page;
     public readonly contentView: Locator;
     public readonly createFlowButton: Locator;
     public readonly firstFlowName: Locator;
