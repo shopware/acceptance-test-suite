796f49e866666d957d520519a0ef616a519cdc05
Yusuf Tür <yusufttur@gmail.com>
2025-08-22 21:52:02 +0200

    fix: use b2b-test-data

--- tests/B2B-Test-Data.spec.ts
+++ tests/B2B-Test-Data.spec.ts
@@ -5,7 +5,7 @@ test('Create 10K business partners with 1K employees each (10 workers)', { tag: 
     test.setTimeout(1440000000); // 16 days
     await TestDataService.setCleanUp(false);
 
-    let nextPartnerId = 141;
+    let nextPartnerId = 2550;
     const totalPartners = 10000;
     const mutex: { locked: boolean; queue: Array<(value?: unknown) => void> } = { locked: false, queue: [] };
 
