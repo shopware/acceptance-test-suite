bd2400103ad9791fa9f3ec3385482080e965b13b
Daniel Meyer <d.meyer@shopware.com>
2025-09-05 12:06:01 +0200

    delete blank lines

--- src/page-objects/administration/ThemesListing.ts
+++ src/page-objects/administration/ThemesListing.ts
@@ -7,12 +7,10 @@ export class ThemesListing implements PageObject {
     public readonly page: Page;
     public readonly installedTheme: (title: string) => Locator;
 
-
     constructor(page: Page) {
         this.page = page;
         this.contentView = page.locator('.sw-desktop__content');
         this.installedTheme = (title: string) => page.locator('.sw-theme-list-item__info', { hasText: title });
-
     }
     
     url() {
