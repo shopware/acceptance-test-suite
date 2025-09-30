325bc664e5028463aeb351dc15c9cec2a0d42496
Florian Robel <f.robel@shopware.com>
2025-09-30 08:26:20 +0200

    fix: export IdProvider

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ export * from './services/ImageHelper';
 export * from './types/ShopwareTypes';
 export * from './services/TestDataService';
 export * from './services/VisualTestHelpers';
+export * from './services/IdProvider';
 
 export { StorefrontPageObjects } from './page-objects/StorefrontPages';
 export { AdminPageObjects } from './page-objects/AdministrationPages';
