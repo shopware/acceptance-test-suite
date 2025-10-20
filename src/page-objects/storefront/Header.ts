b44b499b6d0af01dbf6725ad60fb4bb59ceacdd9
a.okko <14863712+ravenokko@users.noreply.github.com>
2025-10-20 14:37:38 +0200

    test: update locators for Home and Header page objects

--- src/page-objects/storefront/Header.ts
+++ src/page-objects/storefront/Header.ts
@@ -7,6 +7,10 @@ export class Header implements PageObject {
     public readonly languagesMenuOptions: Locator;
     public readonly currenciesDropdown: Locator;
     public readonly currenciesMenuOptions: Locator;
+    public readonly searchInput: Locator;
+
+    //skip links
+    public readonly skipToMainContentLink: Locator;
 
     //wishlist
     public readonly wishlistIcon: Locator;
@@ -19,7 +23,11 @@ export class Header implements PageObject {
         this.languagesDropdown = page.locator('.top-bar-language').filter({ has: page.getByRole('button') });
         this.languagesMenuOptions = page.locator('.top-bar-language').filter({ has: page.getByRole('list') });
         this.currenciesDropdown = page.locator('.top-bar-currency').filter({ has: page.getByRole('button') });
-        this.currenciesMenuOptions = page.locator('.top-bar-currency').filter({ has: page.getByRole('list') });        
+        this.currenciesMenuOptions = page.locator('.top-bar-currency').filter({ has: page.getByRole('list') });
+        this.searchInput = page.getByRole('combobox', { name: 'Search' });        
+
+        //skip links
+        this.skipToMainContentLink = page.getByRole('link', { name: 'Skip to main content' });
 
         //wishlist
         this.wishlistIcon = page.locator('.header-wishlist-icon');
