9076b79409678d46d5851064aadc816d89dbec80
Florian Robel <f.robel@shopware.com>
2025-11-14 11:47:10 +0100

    fix: add translation and locators

--- src/page-objects/administration/DataSharingConsentModal.ts
+++ src/page-objects/administration/DataSharingConsentModal.ts
@@ -1,15 +1,39 @@
-import type { Page } from 'playwright-core';
+import type { Locator, Page } from 'playwright-core';
 import type { PageObject } from '../../types/PageObject';
-import type { HelperFixtureTypes } from '../../fixtures/HelperFixtures';
+import { translate } from '../../services/LanguageHelper';
 
 export class DataSharingConsentModal implements PageObject {
 
     public readonly page: Page;
-    public readonly instanceMeta: HelperFixtureTypes['InstanceMeta'];
+    public readonly shareAllButton: Locator;
+    public readonly shareNothingButton: Locator;
+    public readonly savePreferencesButton: Locator;
+    public readonly shareStoreDataHeadline: Locator;
+    public readonly shareStoreDataText: Locator;
+    public readonly shareStoreDataCheckbox: Locator;
+    public readonly shareUserTrackingDataHeadline: Locator;
+    public readonly shareUserTrackingDataText: Locator;
+    public readonly shareUserTrackingDataCheckbox: Locator;
+    public readonly dataUseDetailsLink: Locator;
+    public readonly privacyPolicyLink: Locator;
 
-    constructor(page: Page, instanceMeta: HelperFixtureTypes['InstanceMeta']) {
+    constructor(page: Page) {
         this.page = page;
-        this.instanceMeta = instanceMeta;
+
+        this.shareAllButton = page.getByRole('button', { name: translate('administration:dashboard:dataSharingModal.shareAll') });
+        this.shareNothingButton = page.getByRole('button', { name: translate('administration:dashboard:dataSharingModal.shareAll') });
+        this.savePreferencesButton = page.getByRole('button', { name: translate('administration:dashboard:dataSharingModal.savePreferences') });
+
+        this.shareStoreDataHeadline = page.getByRole('heading', { name: translate('administration:dashboard:dataSharingModal.shareStoreDataHeadline') });
+        this.shareStoreDataText = page.getByText(translate('administration:dashboard:dataSharingModal.shareStoreDataText'));
+        this.shareStoreDataCheckbox = page.getByRole('checkbox', { name: translate('administration:dashboard:dataSharingModal.shareStoreDataCheckbox') });
+
+        this.shareUserTrackingDataHeadline = page.getByRole('heading', { name: translate('administration:dashboard:dataSharingModal.shareUserTrackingDataHeadline') });
+        this.shareUserTrackingDataText = page.getByText(translate('administration:dashboard:dataSharingModal.shareUserTrackingDataText'));
+        this.shareUserTrackingDataCheckbox = page.getByRole('checkbox', { name: translate('administration:dashboard:dataSharingModal.shareUserTrackingDataCheckbox') });
+
+        this.dataUseDetailsLink = page.getByText(translate('administration:dashboard:dataSharingModal.dataUseDetailsLinkText'));
+        this.privacyPolicyLink = page.getByText(translate('administration:dashboard:dataSharingModal.privacyPolicyLinkText'));
 
     }
 
