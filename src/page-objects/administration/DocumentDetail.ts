35297ba0e10d31bb4ed6a9f77d1303ffda3828b5
Lars Kemper <l.kemper@shopware.com>
2026-02-12 08:36:25 +0100

    fix: documents ats (#582)

--- src/page-objects/administration/DocumentDetail.ts
+++ src/page-objects/administration/DocumentDetail.ts
@@ -3,7 +3,9 @@ import type { PageObject } from "../../types/PageObject";
 import { translate } from "../../services/LanguageHelper";
 
 export class DocumentDetail implements PageObject {
+    /** @deprecated - Use 'displayDocumentInMyAccountSwitch' instead. */
     public readonly showInAccountSwitch: Locator;
+    public readonly displayDocumentInMyAccountSwitch: Locator;
     public readonly saveButton: Locator;
     public readonly documentTypeSelect: Locator;
     public readonly page: Page;
@@ -11,6 +13,7 @@ export class DocumentDetail implements PageObject {
     constructor(page: Page) {
         this.page = page;
         this.showInAccountSwitch = page.getByRole("checkbox", { name: translate("administration:document:detail.displayDocumentInMyAccount") });
+        this.displayDocumentInMyAccountSwitch = page.getByRole("checkbox", { name: translate("administration:document:detail.displayDocumentInMyAccountSwitch") });
         this.saveButton = page.getByRole("button", { name: translate("administration:document:detail.save") });
         this.documentTypeSelect = page.locator(".sw-settings-document-detail__select-type");
     }
