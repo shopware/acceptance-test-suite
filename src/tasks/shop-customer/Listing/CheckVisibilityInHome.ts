ede5e5717d228d4a8c385754b282958ee0ddde10
Jonas Elfering <j.elfering@shopware.com>
2026-02-25 13:39:41 +0100

    Do not clear cache and retry in CheckVisibilityInHome anymore

--- src/tasks/shop-customer/Listing/CheckVisibilityInHome.ts
+++ src/tasks/shop-customer/Listing/CheckVisibilityInHome.ts
@@ -3,18 +3,13 @@ import type { Task } from "../../../types/Task";
 import type { FixtureTypes } from "../../../types/FixtureTypes";
 
 export const CheckVisibilityInHome = base.extend<{ CheckVisibilityInHome: Task } & FixtureTypes>({
-    CheckVisibilityInHome: async ({ ShopCustomer, StorefrontHome, TestDataService }, use) => {
+    CheckVisibilityInHome: async ({ ShopCustomer, StorefrontHome }, use) => {
         const task = (productName: string) => {
             return async function CheckVisibilityInHome() {
-                await TestDataService.clearCaches();
                 const productLocators = await StorefrontHome.getListingItemByProductName(productName);
 
-                await ShopCustomer.expects(async () => {
-                    await ShopCustomer.goesTo(`${StorefrontHome.url()}?a=${Date.now()}`);
-                    await ShopCustomer.expects(productLocators.productName).toBeVisible();
-                }).toPass({
-                    intervals: [1_000, 2_500], // retry after 1 seconds, then every 2.5 seconds
-                });
+                await ShopCustomer.goesTo(StorefrontHome.url());
+                await ShopCustomer.expects(productLocators.productName).toBeVisible();
             };
         };
 
