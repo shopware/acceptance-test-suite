afa68393c2abc3c4ad80fb8af82ec5b98d70b703
a.okko <14863712+ravenokko@users.noreply.github.com>
2026-04-13 18:27:49 +0200

    style: remove extra space

--- tests/PageObjects/AdministrationSettings.spec.ts
+++ tests/PageObjects/AdministrationSettings.spec.ts
@@ -39,7 +39,6 @@ test("Administration page objects - Settings.", async ({
 
     const flowId = await getFlowId("Order enters status cancelled", AdminApiContext);
     await ShopAdmin.goesTo(AdminFlowBuilderDetail.url(flowId)); 
-
     await ShopAdmin.expects(AdminFlowBuilderDetail.saveButton).toBeVisible();
 
     // todo: fix unsaved changes issue on the previous page so that we don't have to do a hard reload
