e0f786c6319140992b2896bf720a0c98e3763d77
Florian Robel <f.robel@shopware.com>
2026-04-14 15:32:22 +0200

    adjust dataSharing page obkect locators for version contraint

--- src/page-objects/administration/DataSharing.ts
+++ src/page-objects/administration/DataSharing.ts
@@ -5,41 +5,42 @@ import { translate } from "../../services/LanguageHelper";
 import { satisfies } from "compare-versions";
 
 export class DataSharing implements PageObject {
-    public readonly dataConsentHeadline: Locator;
+    public readonly dataConsentHeadline: Locator | undefined;
 
-    public readonly dataSharingSuccessMessageLabel: Locator;
-    public readonly dataSharingAgreeButton: Locator;
-    public readonly dataSharingDisableButton: Locator;
-    public readonly dataSharingTermsAgreementLabel: Locator;
+    public readonly dataSharingSuccessMessageLabel: Locator | undefined;
+    public readonly dataSharingAgreeButton: Locator | undefined;
+    public readonly dataSharingDisableButton: Locator | undefined;
+    public readonly dataSharingTermsAgreementLabel: Locator | undefined;
     public readonly page: Page;
     public readonly instanceMeta: HelperFixtureTypes["InstanceMeta"];
 
-    public readonly dataSharingCardTitle: Locator;
-    public readonly dataSharingStoreDataHeadline: Locator;
-    public readonly dataSharingStoreDataCheckbox: Locator;
-    public readonly dataUseDetailsLink: Locator;
-    public readonly privacyPolicyLink: Locator;
+    public readonly dataSharingCardTitle: Locator | undefined;
+    public readonly dataSharingStoreDataHeadline: Locator | undefined;
+    public readonly dataSharingStoreDataCheckbox: Locator | undefined;
+    public readonly dataUseDetailsLink: Locator | undefined;
 
-    constructor(page: Page, instanceMeta: HelperFixtureTypes['InstanceMeta']) {
+    constructor(page: Page, instanceMeta: HelperFixtureTypes["InstanceMeta"]) {
         this.page = page;
         this.instanceMeta = instanceMeta;
-        if (satisfies(instanceMeta.version, "<6.6.1")) {
-            this.dataConsentHeadline = page.locator("header.sw-usage-data-consent-banner__title");
+
+        if (satisfies(instanceMeta.version, "<6.7.9.0")) {
+            if (satisfies(instanceMeta.version, "<6.6.1")) {
+                this.dataConsentHeadline = page.locator("header.sw-usage-data-consent-banner__title");
+            } else {
+                this.dataConsentHeadline = page.locator("h3.sw-usage-data-consent-banner__content-headline");
+            }
+
+            this.dataSharingAgreeButton = page.getByRole("button", { name: translate("administration:dataSharing:buttons.agree") });
+            this.dataSharingDisableButton = page.getByRole("button", { name: translate("administration:dataSharing:buttons.disableDataSharing") });
+            this.dataSharingSuccessMessageLabel = page.getByText(translate("administration:dataSharing:messages.sharingData"), { exact: true });
+            this.dataSharingTermsAgreementLabel = page.getByText(translate("administration:dataSharing:messages.termsAgreement"));
         } else {
-            this.dataConsentHeadline = page.locator("h3.sw-usage-data-consent-banner__content-headline");
+            this.dataSharingCardTitle = page.getByRole("heading", { name: translate("administration:dataSharing:headlines.consentModal") });
+            this.dataSharingStoreDataHeadline = page.getByRole("heading", { name: translate("administration:dataSharing:headlines.storeData") });
+            this.dataSharingStoreDataCheckbox = page.getByRole("checkbox", { name: translate("administration:dataSharing:checkboxes.shareStoreData") });
+
+            this.dataUseDetailsLink = page.getByRole("link", { name: translate("administration:dataSharing:links.storeDataCollectionDetails") });
         }
-
-        this.dataSharingAgreeButton = page.getByRole("button", { name: translate("administration:dataSharing:buttons.agree") });
-        this.dataSharingDisableButton = page.getByRole("button", { name: translate("administration:dataSharing:buttons.disableDataSharing") });
-        this.dataSharingSuccessMessageLabel = page.getByText(translate("administration:dataSharing:messages.sharingData"), { exact: true });
-        this.dataSharingTermsAgreementLabel = page.getByText(translate("administration:dataSharing:messages.termsAgreement"));
-
-        this.dataSharingCardTitle = page.getByRole("heading", { name: "Help us improve" });
-        this.dataSharingStoreDataHeadline = page.getByRole("heading", { name: "Store data" });
-        this.dataSharingStoreDataCheckbox = page.getByRole("checkbox", { name: "Share store data" });
-
-        this.dataUseDetailsLink = page.getByRole("link", { name: "Privacy Policy" });
-        this.privacyPolicyLink = page.getByRole("link", { name: "shopware.com/en/data-use-details" });
     }
 
     url() {
