eada6d412e6cb2570f482da5eeab47da8882fdb3
suri.ta <l.ta@shopware.com>
2026-06-21 18:40:50 +0700

    fix: lint typecheck failed on rule

--- src/services/TestDataService.ts
+++ src/services/TestDataService.ts
@@ -832,7 +832,7 @@ export class TestDataService {
             personaRules: [
                 {
                     id: promotionConfig.ruleId,
-                } as Partial<Rule>,
+                } as Pick<Rule, "id">,
             ],
         });
 
