2dafe11c86bc96ab0d4581efc9863f8d299950f6
suri.ta <l.ta@shopware.com>
2026-06-26 12:43:03 +0700

    fix: add overrides into the createNewsletterRecipient and fix other PR comments

--- src/page-objects/storefront/OffCanvasCart.ts
+++ src/page-objects/storefront/OffCanvasCart.ts
@@ -70,7 +70,7 @@ export class OffCanvasCart implements PageObject {
     }
 
     async getLineItemByPromotionName(name: string): Promise<Record<string, Locator>> {
-        let promotionItem = this.page.locator(".line-item-promotion", { hasText: name });
+        const promotionItem = this.page.locator(".line-item-promotion", { hasText: name });
         const promotionLabel = promotionItem.locator(".line-item-label");
         const promotionPrice = promotionItem.locator(".line-item-total-price-value");
 
