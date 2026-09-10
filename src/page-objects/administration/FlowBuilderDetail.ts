913392b5d7b0087b8da6757a1aee2d6fbd1368ec
Juri Himmelreich <j.himmelreich@shopware.com>
2026-09-10 14:40:38 +0200

    fix(flow): use mt-tabs locators for flow detail tabs behind v6.8.0.0 (#698)

--- src/page-objects/administration/FlowBuilderDetail.ts
+++ src/page-objects/administration/FlowBuilderDetail.ts
@@ -21,7 +21,15 @@ export class FlowBuilderDetail extends FlowBuilderCreate implements PageObject {
 
     constructor(page: Page, instanceMeta: HelperFixtureTypes["InstanceMeta"]) {
         super(page, instanceMeta);
-        this.generalTab = page.locator(".sw-flow-detail__tab-general");
+        //TODO: After 6.8.0.0 release if condition needs to be replaced with 'satisfies(instanceMeta.version, ">=6.8")'
+        if (instanceMeta.features["V6_8_0_0"]) {
+            this.generalTab = page.locator('.mt-tabs__item[data-item-name="sw.flow.detail.general"]');
+            this.flowTab = page.locator('.mt-tabs__item[data-item-name="sw.flow.detail.flow"]');
+        } else {
+            this.generalTab = page.locator(".sw-flow-detail__tab-general");
+            this.flowTab = page.locator(".sw-flow-detail__tab-flow");
+        }
+        //TODO: After 6.8.0.0 release this condition can be removed. Only the else branch is needed then
         if (satisfies(instanceMeta.version, "<6.7")) {
             this.successMessage = page.locator(".sw-alert__title");
             this.saveButtonLoader = page.locator(".sw-button--primary").locator(".sw-button_loader");
@@ -34,7 +42,6 @@ export class FlowBuilderDetail extends FlowBuilderCreate implements PageObject {
             this.alertMessage = page.locator(".mt-banner__title");
         }
         this.saveButton = page.locator(".sw-flow-detail__save");
-        this.flowTab = page.locator(".sw-flow-detail__tab-flow");
         this.templateName = page.getByLabel(translate("administration:flowBuilder:detail.name"));
         this.actionContentTag = page.locator(".sw-flow-sequence-action__content").locator(".tag");
         this.skeletonLoader = page.locator(".sw-skeleton");
